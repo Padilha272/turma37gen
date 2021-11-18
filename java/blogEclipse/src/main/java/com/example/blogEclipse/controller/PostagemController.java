01b80c80b7feae2f0fea469cddacb9239ca29d5d
Padilha272 <matheuspadilha48@hotmail.com>
2021-11-17 22:03:29 -0300

    Atualizacao-blog-eclipse-17-11-21

--- java/blogEclipse/src/main/java/com/example/blogEclipse/controller/PostagemController.java
+++ java/blogEclipse/src/main/java/com/example/blogEclipse/controller/PostagemController.java
@@ -1,8 +1,61 @@
 package com.example.blogEclipse.controller;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-public class PostagemController {
+import com.example.blogEclipse.model.Postagem;
+import com.example.blogEclipse.repository.PostagemRepository;
 
+@RestController
+@RequestMapping("/postagens")
+@CrossOrigin("*")
+public class PostagemController {
+	
+	@Autowired
+	private PostagemRepository repository;
+	
+	@GetMapping
+	public ResponseEntity <List<Postagem>> GetAll(){
+		
+		return ResponseEntity.ok(repository.findAll());
+	}
+	
+	@GetMapping("/{id}")
+	public ResponseEntity<Optional<Postagem>> getById(@PathVariable Long id){
+		
+		return ResponseEntity.ok(repository.findById(id));
+	}
+	
+	@GetMapping("/titulo/{titulo}")
+	
+	public ResponseEntity <List<Postagem>> getByTitulo(@PathVariable String titulo){
+		
+		return ResponseEntity.ok(repository.findAllByTituloContainingIgnoreCase(titulo));
+	
+	}
+	
+	
+	@GetMapping("/autor/{autor}")
+	
+	public ResponseEntity<List<Postagem>> getByAutor(@PathVariable String autor) {
+		
+		return ResponseEntity.ok(repository.findAllByAutorContainingIgnoreCase(autor));
+	}
+	
+	
+	
+	
 }
--- java/blogEclipse/src/main/java/com/example/blogEclipse/repository/PostagemRepository.java
+++ java/blogEclipse/src/main/java/com/example/blogEclipse/repository/PostagemRepository.java
@@ -1,7 +1,7 @@
 package com.example.blogEclipse.repository;
 
 import java.util.List;
-
+import java.util.Optional;
 
 import org.springframework.stereotype.Repository;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -10,4 +10,6 @@ import com.example.blogEclipse.model.Postagem;
 @Repository
 public interface PostagemRepository extends JpaRepository<Postagem,Long>{
 	public List<Postagem> findAllByTituloContainingIgnoreCase (String titulo);
+
+	public  List<Postagem> findAllByAutorContainingIgnoreCase(String autor);
 }
