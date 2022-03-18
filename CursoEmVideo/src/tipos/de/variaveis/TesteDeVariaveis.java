7ac7ab2ab433533c638a6c98d9568ab78b3ac606
Matheus Padilha <matheuspadilha48@hotmail.com>
2022-03-18 11:33:50 -0300

    Estudo-18-03-22

---
+++ CursoEmVideo/src/tipos/de/variaveis/TesteDeVariaveis.java
@@ -0,0 +1,19 @@
+package tipos.de.variaveis;
+
+public class TesteDeVariaveis {
+
+	public static void main(String[] args) {
+		
+		
+		
+		int idade = 5;
+		System.out.println(idade);
+	
+		
+		
+		
+		
+
+	}
+
+}
